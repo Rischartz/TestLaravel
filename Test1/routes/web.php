a1db20adf42a6220d1e059af0b8364edcc2eeff3
JlSystemsMex <jlsystemsmex@hotmail.com>
2023-02-07 16:11:44 -0600

    Prueba rutas

--- Test1/routes/web.php
+++ Test1/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/servicios', function () {
+    return 'Página de servicios';
+});
